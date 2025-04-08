28b67852aad5d0819624b7143b54b37c0b19a809
Mohamed Sahel <mosahel000@gmail.com>
2025-04-08 15:50:53 +0530

    commit

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -5,24 +5,13 @@ export const socials = [
     id: "0",
     title: "x",
     icon: "/images/socials/x.svg",
-    url: "#",
+    url: "https://x.com/LetsTalkB2B",
   },
   {
     id: "1",
     title: "Linkedin",
     icon: "/images/socials/linkedin.svg",
-    url: "#",
+    url: "https://www.linkedin.com/showcase/mebankingai/",
   },
-  {
-    id: "2",
-    title: "Instagram",
-    icon: "/images/socials/instagram.svg",
-    url: "#",
-  },
-  {
-    id: "3",
-    title: "Facebook",
-    icon: "/images/socials/facebook.svg",
-    url: "#",
-  },
+ 
 ];
